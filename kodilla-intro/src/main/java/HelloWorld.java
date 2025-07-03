06fbef434e7e19c4857395413cb350b96fdeba88
Sabir Ayyubov <sabirayyubov67@gmail.com>
2025-07-03 02:15:14 +0200

    quest 1

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("'Hello World!' greeting");
+        System.out.println("My name is Sabir");
+        System.out.println("My age is 19");
+        System.out.println("My first letter of my last name is 'A'");
+    }
+}
