2e7bdcabe4d951b763bd8539adc4782780a6bc64
Sabir Ayyubov <sabirayyubov67@gmail.com>
2025-07-03 18:47:26 +0200

    module variables

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,12 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myFullName = "Sabir Ayyubov";
+        int birthYear = 2006;
+        String residanceCity = "Krakow";
+        String carName = "Toyota";
+        boolean isGoodDriver = true;
+        boolean isStudent = true;
+
+        System.out.println(myFullName);
+    }
+}
---
+++ kodilla-intro/src/main/java/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        String myName = "Sabir";
+        int myAge = 19;
+        double myHeight = 1.84;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+    }
+}
