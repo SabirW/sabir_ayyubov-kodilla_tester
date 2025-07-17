94808b29551efe4801c47e11964fb566ce0b40ba
Sabir Ayyubov <sabirayyubov67@gmail.com>
2025-07-17 03:48:49 +0200

    statics

---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,21 @@
+public class Book {
+
+    private String author;
+    private String title;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public static Book of(String author, String title) {
+        this.author = author;
+        this.title = title;
+        return new Book(author, title);
+
+    }
+
+    public static void main(String[] args) {
+        Book book = Book.of("Harry Potter", "JK Rowling");
+    }
+}
